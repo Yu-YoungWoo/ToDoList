65f980b0ca8418e7191c68f7a485fcd7b7d4c484
LastChallenge <LastChallenged@gmail.com>
2021-04-18 17:36:49 +0900

    fixed some bugs

--- ToDo.js
+++ ToDo.js
@@ -1,17 +1,19 @@
+const toDoForm = document.querySelector('.form-add');
 const getToDo = document.querySelector('.todo-input');  //input
 const addToDo = document.querySelector('.add-todo');  // buttons
 const todoList = document.querySelector('.list-set');   //ul
-const todoItem = document.querySelectorAll('.list-set li');  //li 
 
-let listLength = todoList.length;
+const TODOS_Ls = "toDos";
+let todoArr = [];
 
+let newId = todoArr.length;
 
 const generateTempalate = (todo) => {
     const item = 
     ` 
     <li>
-        <input type="checkbox" id="todo_${listLength}">
-        <label for="todo_${listLength}">
+        <input type="checkbox" id = "${newId - 1}">
+        <label for="${newId - 1}">
         <span class="check"></span>
             ${todo}
         </label>
@@ -22,17 +24,65 @@ const generateTempalate = (todo) => {
     todoList.innerHTML += item;
 };
 
-function addTodos(e) {
-    e.preventDefault();
-    const todo = getToDo.value; //remove white space
-    if(todo.length) {
-        listLength += 1;
-        $('input').keyup(function(e) {
-            if(e.keyCode == 13) addTodos(e);
-        });        
-        generateTempalate(todo);
-        getToDo.value = "";
+function addTodos(text) {
+    if(text) {
+        const toDoObj = {
+            text,
+            id: newId,
+        };
+        newId += 1;
+        todoArr.push(toDoObj);  //localStorage Array
+        saveToDos();        // save localStorage
+        generateTempalate(text);
     }
 }
-getToDo.addEventListener('submit', addTodos);
-addToDo.addEventListener('click', addTodos);
+
+function deleteTodos(e) {
+    
+    const cleanToDos = todoArr.filter(todoArr => {
+        return todoArr.id !== parseInt(e.target.parentElement.id);
+    });
+    todoArr = cleanToDos;
+    saveToDos();
+    
+    
+    if(e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+    
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = getToDo.value;
+    addTodos(currentValue);
+    getToDo.value = "";
+}
+
+function saveToDos() {
+    localStorage.setItem(TODOS_Ls, JSON.stringify(todoArr));
+}
+
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_Ls);
+    
+    if(loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+
+        parsedToDos.forEach(function (toDo) {
+            addTodos(toDo.text);
+        });
+    }
+}
+
+
+function init() {
+    loadToDos();
+
+    toDoForm.addEventListener("submit", handleSubmit);
+    addToDo.addEventListener("click", handleSubmit);
+    todoList.addEventListener("click", deleteTodos);
+}
+
+init();
+
